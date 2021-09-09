6fce3d3019c5e109e723442688a534e5d2802949
prajwal-u-patil <prajwalp@mkcl.org>
2021-09-09 09:07:14 +0530

    ticket field added

--- SOURCE_API/models/models.go
+++ SOURCE_API/models/models.go
@@ -11,6 +11,7 @@ type Source struct {
 	DeveloperName string             `json:"devname,omitempty" bson:"devname,omitempty"`
 	Email         string             `json:"email,omitempty" bson:"email,omitempty"`
 	SourceLink    string             `json:"sourcelink,omitempty" bson:"sourcelink,omitempty"`
+	Ticket        string             `json:"ticket,omitempty" bson:"ticket,omitempty"`
 	Timestamp     time.Time          `json:"timestamp,omitempty" bson:"timestamp,omitempty"`
 }
 type Deployment struct {
